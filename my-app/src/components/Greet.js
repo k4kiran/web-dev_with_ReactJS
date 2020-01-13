2924b2c2d558f69b5e79f4689f3e2712c864b42c
k4kiran <kforkiran005@gmail.com>
2020-01-13 16:19:22 +0530

    first project

---
+++ my-app/src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//using the js function
+/*
+function Greet(){
+    return <h1>Welcome Kiran</h1>
+}
+*/
+
+//using ES6 standard
+const Greet = () => <h1>Welcome Kiran</h1>
+export default Greet
